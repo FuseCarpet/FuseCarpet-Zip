2ecd72d109ebe5e6b2f163da2d5dc2ceb59348a5
eddy2kb <99700494+eddy2kb@users.noreply.github.com>
2022-09-13 20:32:23 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,6 +6,12 @@ import easygui
 import pyperclip
 import os
 
+gui.terms2(terms='''By clicking "I agree", you agree to these terms:
+
+    1. No distributing copies of this software on anywhere other than https://github.com/FuseCarpet unless allowed by rule 2.
+    2. You are allowed to distribute this software IF IT IS MODIFIED, with credits: Original made by FuseCarpet at https://github.com/FuseCarpet
+''')
+
 class _status:
     en_open = False
     de_open = False
